fbb35dc72975b72233e97b2a9b663122bb6895f0
mrShahid <a.andrey.victorovich@gmail.com>
2017-04-20 10:06:09 +0200

    Aggregator 15

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -2,7 +2,12 @@ package com.javarush.task.task28.task2810.view;
 
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
@@ -14,6 +19,7 @@ public class HtmlView implements View {
 
     private Controller controller;
     private final String filePath = "./src/" + getClass().getPackage().getName().replaceAll("\\.", "/") + "/vacancies.html";
+    //private final String filePath = "c:\\Users\\Andrey\\JavaRushHomeWork\\JavaRushHomeWork\\src\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task28\\task2810\\view\\vacancies.html";
 
     @Override
     public void update(List<Vacancy> vacancies) {
@@ -35,8 +41,54 @@ public class HtmlView implements View {
     }
 
     private String getUpdatedFileContent(List<Vacancy> list){
-        return null;
+        Document doc;
+        try {
+            doc = getDocument();
+
+            Elements elementVacancy = doc.getElementsByAttributeValue("class", "vacancy");
+            for (int i=0;i<elementVacancy.size();i++){
+                elementVacancy.get(i).remove();
+            }
+
+            Element templateElement = doc.getElementsByClass("template").first();
+            Element cloneTemplateElement = templateElement.clone();
+            cloneTemplateElement.removeClass("template");
+            cloneTemplateElement.removeAttr("style");
+
+            Vacancy vacancy;
+
+            for (int i=0;i<list.size();i++){
+                vacancy = list.get(i);
+                Element e = cloneTemplateElement.clone();
+
+                //add city
+               e.getElementsByClass("city").first().text(vacancy.getCity());
+                // add companyName
+                e.getElementsByClass("companyName").first().text(vacancy.getCompanyName());
+                // add salary
+                e.getElementsByClass("salary").first().text(vacancy.getSalary());
+                //add url
+                Element elementLink = e.getElementsByClass("title").first();
+                elementLink.text(vacancy.getTitle());
+                elementLink.attr("href", vacancy.getUrl());
+
+                templateElement.before(e.outerHtml());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Some exception occurred";
+
+
+
+        }
+        return doc.toString();
     }
+
+    protected Document getDocument() throws IOException{
+
+        return Jsoup.parse(new File(filePath), "UTF-8");
+    }
+
 
     private void updateFile(String s) {
 
